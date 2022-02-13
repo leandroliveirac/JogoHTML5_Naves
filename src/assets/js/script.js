5abf84799b4cba4becc96b48d7803f87fc6f35c9
leandroliveirac <leandroparipe@hotmail.com>
2022-02-13 18:37:58 -0300

    Detectar tecla pressionada

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -9,8 +9,26 @@ function start(){
 
     //Principais variáveis do jogo
         
-    var jogo = {}
-        
+    var jogo = {};
+	var TECLA = {
+        W: 87,
+        S: 83,
+        D: 68
+    }
+    
+    jogo.pressionou = [];
+
+    //Verifica se o usuário pressionou alguma tecla	
+
+    $(document).keydown(function(e){
+    jogo.pressionou[e.which] = true;
+    });
+
+
+    $(document).keyup(function(e){
+        jogo.pressionou[e.which] = false;
+    });
+
     //Game Loop
 
     jogo.timer = setInterval(loop,30);
@@ -18,6 +36,7 @@ function start(){
     function loop() {
 
         movefundo();
+        movejogador();
 
     } // Fim da função loop()
 
@@ -29,5 +48,26 @@ function start(){
         $("#fundoGame").css("background-position",esquerda-1);
         
     } // fim da função movefundo()
+
+    function movejogador() {
+	
+        if (jogo.pressionou[TECLA.W]) {
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top",topo-10);
+        
+        }
+        
+        if (jogo.pressionou[TECLA.S]) {
+            
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top",topo+10);	
+        }
+        
+        if (jogo.pressionou[TECLA.D]) {
+            
+            //Chama função Disparo	
+        }
+    
+        } // fim da função movejogador()
 }
 
