7892b2e7cf8ad21209bd12fb24e115cf555b525d
leandroliveirac <leandroparipe@hotmail.com>
2022-02-13 20:17:38 -0300

    Adicionando a funcionalidade do tiro para o jogador

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -17,7 +17,8 @@ function start(){
     }
     var velocidade = 5;
     var posicaoY = parseInt(Math.random() * 334);
-    
+    var podeAtirar=true;
+
     jogo.pressionou = [];
 
     //Verifica se o usuário pressionou alguma tecla	
@@ -77,7 +78,8 @@ function start(){
         
         if (jogo.pressionou[TECLA.D]) {
             
-            //Chama função Disparo	
+            //Chama função Disparo
+            disparo();	
         }
     
     } // fim da função movejogador()
@@ -116,5 +118,38 @@ function start(){
         }
 
     } // fim da função moveamigo()
+
+    function disparo() {
+	
+        if(podeAtirar==true) {
+            
+            podeAtirar=false;
+            
+            topo = parseInt($("#jogador").css("top"))
+            posicaoX= parseInt($("#jogador").css("left"))
+            tiroX = posicaoX + 190;
+            topoTiro=topo+37;
+            $("#fundoGame").append("<div id='disparo'></div");
+            $("#disparo").css("top",topoTiro);
+            $("#disparo").css("left",tiroX);
+            
+            var tempoDisparo=window.setInterval(executaDisparo, 30);
+        
+        } //Fecha podeAtirar
+     
+        function executaDisparo() {
+            posicaoX = parseInt($("#disparo").css("left"));
+            $("#disparo").css("left",posicaoX+15); 
+    
+            if (posicaoX>900) {
+                            
+                window.clearInterval(tempoDisparo);
+                tempoDisparo=null;
+                $("#disparo").remove();
+                podeAtirar=true;
+                    
+            }
+        } // Fecha executaDisparo()
+    } // Fecha disparo()
 }
 
