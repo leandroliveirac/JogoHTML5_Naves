e864342a69e56015deaa3d212429ef57d48480be
leandroliveirac <leandroparipe@hotmail.com>
2022-02-13 22:23:23 -0300

    adicionando efeito sonoro

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -29,6 +29,17 @@ function start(){
 
     jogo.pressionou = [];
 
+    var somDisparo=document.getElementById("somDisparo");
+    var somExplosao=document.getElementById("somExplosao");
+    var musica=document.getElementById("musica");
+    var somGameover=document.getElementById("somGameover");
+    var somPerdido=document.getElementById("somPerdido");
+    var somResgate=document.getElementById("somResgate");
+    
+    //Música em loop
+    musica.addEventListener("ended", function(){ musica.currentTime = 0; musica.play(); }, false);
+    musica.play();
+
     //Verifica se o usuário pressionou alguma tecla	
 
     $(document).keydown(function(e){
@@ -134,6 +145,7 @@ function start(){
 	
         if(podeAtirar==true) {
             
+            somDisparo.play();
             podeAtirar=false;
             
             topo = parseInt($("#jogador").css("top"))
@@ -232,6 +244,7 @@ function start(){
 	    if (colisao5.length>0) {
             
             salvos++;
+            somResgate.play();
             reposicionaAmigo();
             $("#amigo").remove();
         }
@@ -254,6 +267,7 @@ function start(){
 
     //Explosão 1
     function explosao1(inimigo1X,inimigo1Y) {
+        somExplosao.play();
         $("#fundoGame").append("<div id='explosao1'></div");
         $("#explosao1").css("background-image", "url(assets/img/explosao.png)");
         var div=$("#explosao1");
@@ -275,7 +289,7 @@ function start(){
     //Explosão2
 	
 	function explosao2(inimigo2X,inimigo2Y) {
-	
+        somExplosao.play();
         $("#fundoGame").append("<div id='explosao2'></div");
         $("#explosao2").css("background-image", "url(assets/img/explosao.png)");
         var div2=$("#explosao2");
@@ -296,6 +310,7 @@ function start(){
     //Explosão3
 	
     function explosao3(amigoX,amigoY) {
+        somPerdido.play();
         $("#fundoGame").append("<div id='explosao3' class='anima4'></div");
         $("#explosao3").css("top",amigoY);
         $("#explosao3").css("left",amigoX);
